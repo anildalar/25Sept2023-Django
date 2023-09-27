e3ca1539038f3c1e8f026351b967ea464204c4e6
anildalar <dollorinfotech@gmail.com>
2023-09-27 09:01:29 +0530

    c4

--- amazon/settings.py
+++ amazon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-27mz)h$9c6elut=@()%ko-sxbg*+5x4w6=yuttira57!jd-g2^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
